391da6694b350bb93d712ed4fe506ae58930b1ed
Vincent Chan <okcdz@diverse.space>
2022-03-09 16:17:46 +0800

    use npm instead of pnpm

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,10 +13,6 @@ import {
   InitializeResult,
 } from 'vscode-languageserver/node';
 
-import {
-  TextDocument
-} from 'vscode-languageserver-textdocument';
-
 // Create a connection for the server, using Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
 const connection = createConnection(ProposedFeatures.all);
