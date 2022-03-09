70dfae22494d26ec21dca0fdf86b9b467ce079f3
Vincent Chan <okcdz@diverse.space>
2022-03-09 22:04:46 +0800

    reduce the size of package

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,16 @@ All notable changes to the "lichenscript-vscode" extension will be documented in
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
-## [Unreleased]
+
+## 0.1.1
+
+- add `*.ts` and `tsconfig.json` to `.vscodeignore`
+
+## 0.1.0
+
+- Support syntax error dectection
+
+## 0.0.x
 
 - Initial release
+- Add keyword to syntax configuration
--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,10 @@ import {
   TextDocumentSyncKind,
   InitializeResult,
   TextDocuments,
-  TextDocumentChangeEvent
+  TextDocumentChangeEvent,
+  TextDocumentPositionParams,
+  CompletionItem,
+  // CompletionItemKind
 } from 'vscode-languageserver/node';
 import { TextDocument } from 'vscode-languageserver-textdocument';
 import { createIntellisenseInstance } from 'lichenscript-web';
@@ -166,41 +169,41 @@ function handleDocumentChanged(e: TextDocumentChangeEvent<TextDocument>) {
 //   connection.console.log('We received an file change event');
 // });
 
-// // This handler provides the initial list of the completion items.
-// connection.onCompletion(
-//   (_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-//     // The pass parameter contains the position of the text document in
-//     // which code complete got requested. For the example we ignore this
-//     // info and always provide the same completion items.
-//     return [
-//       {
-//         label: 'TypeScript',
-//         kind: CompletionItemKind.Text,
-//         data: 1
-//       },
-//       {
-//         label: 'JavaScript',
-//         kind: CompletionItemKind.Text,
-//         data: 2
-//       }
-//     ];
-//   }
-// );
+// This handler provides the initial list of the completion items.
+connection.onCompletion(
+  (_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+    // The pass parameter contains the position of the text document in
+    // which code complete got requested. For the example we ignore this
+    // info and always provide the same completion items.
+    return [
+      // {
+      //   label: 'TypeScript',
+      //   kind: CompletionItemKind.Text,
+      //   data: 1
+      // },
+      // {
+      //   label: 'JavaScript',
+      //   kind: CompletionItemKind.Text,
+      //   data: 2
+      // }
+    ];
+  }
+);
 
-// // This handler resolves additional information for the item selected in
-// // the completion list.
-// connection.onCompletionResolve(
-//   (item: CompletionItem): CompletionItem => {
-//     if (item.data === 1) {
-//       item.detail = 'TypeScript details';
-//       item.documentation = 'TypeScript documentation';
-//     } else if (item.data === 2) {
-//       item.detail = 'JavaScript details';
-//       item.documentation = 'JavaScript documentation';
-//     }
-//     return item;
-//   }
-// );
+// This handler resolves additional information for the item selected in
+// the completion list.
+connection.onCompletionResolve(
+  (item: CompletionItem): CompletionItem => {
+    if (item.data === 1) {
+      item.detail = 'TypeScript details';
+      item.documentation = 'TypeScript documentation';
+    } else if (item.data === 2) {
+      item.detail = 'JavaScript details';
+      item.documentation = 'JavaScript documentation';
+    }
+    return item;
+  }
+);
 
 documents.listen(connection);
 
