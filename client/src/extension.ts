d10275a3babad413a48166aee1f447a3def5b2df
Vincent Chan <okcdz@diverse.space>
2022-03-09 11:59:40 +0800

    feat: config for language

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,6 +1,6 @@
 
 import * as path from 'path';
-import { workspace, ExtensionContext } from 'vscode';
+import { ExtensionContext } from 'vscode';
 
 import {
   LanguageClient,
@@ -33,17 +33,17 @@ export function activate(context: ExtensionContext) {
   // Options to control the language client
   const clientOptions: LanguageClientOptions = {
     // Register the server for plain text documents
-    documentSelector: [{ scheme: 'file', language: 'plaintext' }],
-    synchronize: {
-      // Notify the server about file changes to '.clientrc files contained in the workspace
-      fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-    }
+    documentSelector: [{ language: 'lichenscript' }],
+    // synchronize: {
+    //   // Notify the server about file changes to '.clientrc files contained in the workspace
+    //   fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+    // }
   };
 
   // Create the language client and start the client.
   client = new LanguageClient(
-    'languageServerExample',
-    'Language Server Example',
+    'LichenScriptServer',
+    'LichenScript Language Server',
     serverOptions,
     clientOptions
   );
